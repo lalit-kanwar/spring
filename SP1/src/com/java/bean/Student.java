ba3922e03ee701ee7402f75e063e793c77ef2892
OxygenTL <lalit@lalit-PC>
2020-04-11 22:15:19 +0530

    share SP1 from eclips to remote repository

---
+++ SP1/src/com/java/bean/Student.java
@@ -0,0 +1,23 @@
+package com.java.bean;
+
+public class Student {
+	private int studentId;
+	private String studentName;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+}
